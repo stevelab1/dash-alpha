13e8dd75c0257a983b1ac96512fd34b37815934d
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2023-02-13 18:59:08 +0000

    test test setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
+import Test from './components/Test'
 
 function App() {
 
   return (
     <div data-test="component-app">
+      <Test />
     </div>
   );
 }
---
+++ src/components/Test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div data-test="component-test">
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default Test;
