e6de00c15e00975aa53434bf71f4570b7c1fe2a6
Nick <72912456+nickmbk@users.noreply.github.com>
2023-02-16 20:00:07 +0000

    request antonyms

--- src/components/WordsAPI.js
+++ src/components/WordsAPI.js
@@ -4,7 +4,7 @@ function WordsAPI() {
     const APIKEY = '96ffecab55msh5610f9306675572p1e30cbjsn609e7afc48d1';
     const BASEURL = 'wordsapiv1.p.rapidapi.com';
 
-    // 
+    // get the definitions, type of word, syllables (for pronounciation) and synonyms
     const requestWord = {
         method: 'GET',
         url: `https://wordsapiv1.p.rapidapi.com/words/example`,
@@ -20,7 +20,21 @@ function WordsAPI() {
           console.error(error);
     });
 
-
+    // get antonyms
+    const requestAntonyms = {
+        method: 'GET',
+        url: 'https://wordsapiv1.p.rapidapi.com/words/example/antonyms',
+        headers: {
+          'X-RapidAPI-Key': APIKEY,
+          'X-RapidAPI-Host': BASEURL
+        }
+      };
+      
+      Axios.request(requestAntonyms).then(function (response) {
+          console.log(response.data);
+      }).catch(function (error) {
+          console.error(error);
+      });
 
     return (
         <div>
