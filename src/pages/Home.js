e830b246010fbd86ca420301d04d49a7a6a21e8b
ad0m <adamweldon@gmail.com>
2023-02-16 17:53:09 +0000

    Working on top hero component for searching a word

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
-    <div className="Pages" data-test="home-page">
-      Home
+    <div className="home">
+      <Hero />
     </div>
   );
 };
