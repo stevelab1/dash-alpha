e04587a38d5f7f412f1a37322026894a20aa64fc
Nick <72912456+nickmbk@users.noreply.github.com>
2023-02-16 19:53:31 +0000

    get response from API

--- src/components/WordsAPI.js
+++ src/components/WordsAPI.js
@@ -3,8 +3,8 @@ import Axios from 'axios';
 function WordsAPI() {
     const APIKEY = '96ffecab55msh5610f9306675572p1e30cbjsn609e7afc48d1';
     const BASEURL = 'wordsapiv1.p.rapidapi.com';
-    const result;
 
+    // 
     const requestWord = {
         method: 'GET',
         url: `https://wordsapiv1.p.rapidapi.com/words/example`,
@@ -15,13 +15,18 @@ function WordsAPI() {
       };
       
       Axios.request(requestWord).then(function (response) {
-          result = (response.data);
+          console.log(response.data);
       }).catch(function (error) {
           console.error(error);
     });
 
+
+
     return (
-        console.log(result)
+        <div>
+        
+        </div>
     )
 }
 
+export default WordsAPI;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import WordsAPI from '../components/WordsAPI';
 
 const Home = () => {
   return (
     <div className="home">
       <Hero />
+      <WordsAPI />
     </div>
   );
 };
