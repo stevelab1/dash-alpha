6e0f5cbf733b11fee529d3d2c32a753e9d4dce15
Nick <72912456+nickmbk@users.noreply.github.com>
2023-02-16 19:04:32 +0000

    create WordAPI component, get data from API

---
+++ src/components/WordsAPI.js
@@ -0,0 +1,27 @@
+import Axios from 'axios';
+
+function WordsAPI() {
+    const APIKEY = '96ffecab55msh5610f9306675572p1e30cbjsn609e7afc48d1';
+    const BASEURL = 'wordsapiv1.p.rapidapi.com';
+    const result;
+
+    const requestWord = {
+        method: 'GET',
+        url: `https://wordsapiv1.p.rapidapi.com/words/example`,
+        headers: {
+          'X-RapidAPI-Key': APIKEY,
+          'X-RapidAPI-Host': BASEURL
+        }
+      };
+      
+      Axios.request(requestWord).then(function (response) {
+          result = (response.data);
+      }).catch(function (error) {
+          console.error(error);
+    });
+
+    return (
+        console.log(result)
+    )
+}
+
