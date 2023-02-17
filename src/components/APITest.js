1e0acb105eb0a2c86bcd2ac804048c6859939aba
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2023-02-17 16:33:17 +0000

    API Test

---
+++ src/components/APITest.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import WordsAPI from "../data/testwordsAPI";
+
+function APITest() {
+  const word = 'example'; // or get the word from user input or elsewhere
+  const [results, setResults] = React.useState(null);
+
+  React.useEffect(() => {
+    const getWordBreakdown = async () => {
+      const wordBreakdown = await WordsAPI(word);
+      setResults(wordBreakdown);
+    };
+
+    getWordBreakdown();
+  }, [word]);
+
+  if (!results) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className='home'>
+      <p>Word: {results.word}</p>
+      <p>Definition: {results.definition}</p>
+      <p>Parts of Speech: {results.partsOfSpeech}</p>
+      <p>Syllables: {results.syllables.join(', ')}</p>
+      <p>Synonyms: {results.synonyms.join(', ')}</p>
+      {results.antonyms && <p>Antonyms: {results.antonyms.join(', ')}</p>}
+    </div>
+  );
+}
+
+export default APITest;
