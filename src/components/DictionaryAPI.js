8fb23060f3d7f3bf5372c6ad19dedb7b4fe2b310
Nick <72912456+nickmbk@users.noreply.github.com>
2023-02-17 18:33:25 +0000

    retrieve data from api

--- src/components/DictionaryAPI.js
+++ src/components/DictionaryAPI.js
@@ -1,7 +1,22 @@
 import Axios from 'axios';
 
 function DictionaryAPI() {
-    
+    const requestWord = {
+        method: 'GET',
+        url: 'https://api.dictionaryapi.dev/api/v2/entries/en/example',
+    };
+
+    Axios.request(requestWord).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+
+    return (
+        <div></div>
+    )
 }
+
+
 
 export default DictionaryAPI;
