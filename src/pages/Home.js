1dd288f61e86b559dc27c0ac20972aa4d899975b
Nick <72912456+nickmbk@users.noreply.github.com>
2023-02-18 17:49:04 +0000

    remove API components from Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,10 @@
 import React from "react";
 import SearchPage from "./SearchPage"
-import DictionaryAPI from '../components/DictionaryAPI';
-import WordsAPI from '../components/WordsAPI';
-import WordDictionaryAPI from '../components/WordDictionaryAPI';
 
 const Home = () => {
   return (
     <div>
       <SearchPage />
-      {/* <DictionaryAPI /> */}
-      <WordDictionaryAPI />
     </div>
   );
 };
