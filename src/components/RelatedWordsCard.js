959e5eb3b995756ead028d926d95a18b85bc52e6
Nick <72912456+nickmbk@users.noreply.github.com>
2023-02-18 20:11:48 +0000

    add bootstrap classes for styling

--- src/components/RelatedWordsCard.js
+++ src/components/RelatedWordsCard.js
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
-import { Card } from "react-bootstrap";
+import { Row, Card } from "react-bootstrap";
 import { SearchContext } from "../context/SearchContext";
 
 function generateList(words) {
     return (words.map((item) => {
         return(
-            <li key={item}>{item}</li>
+            <li className="list-group-item" key={item}>{item}</li>
         )
     })
     )
@@ -20,48 +20,49 @@ function RelatedWordsCard() {
       <div></div>
     )
   } else {
-    console.log(apiStatus);
     // Otherwise, display the synonyms, antonyms and rhyming words
     return (
-      <Card className="related-words-card">
-        <Card.Body>
-          <Card.Title>Related Words</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-          </Card.Subtitle>
-          <Card.Text>{apiStatus.definition}</Card.Text>
-            <div className="d-flex flex-row">
-                {/* rhymes card */}
-                <Card className="rhymes-card">
-                    <Card.Body>
-                        <Card.Title>Rhyming Words</Card.Title>
-                        <ul>
-                            {generateList(apiStatus.rhymes)}
-                        </ul>
-                    </Card.Body>
-                </Card>
+      <Row className="m-3">
+        <Card className="related-words-card col-12 col-lg-12 p-0 mx-auto border-0">
+            <Card.Body className="p-0 m-0">
+            <Card.Title className="p-0 m-3">Words related to {apiStatus.word}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+            </Card.Subtitle>
+                <div className="d-flex flex-column flex-md-row flex-grow-1 justify-content-evenly pb-3">
+                    {/* rhymes card */}
+                    <Card className="rhymes-card col-12 col-md-3 my-2">
+                        <Card.Body>
+                            <Card.Title className="p-0 m-3">Rhyming Words</Card.Title>
+                            <ul className="list-group">
+                                {generateList(apiStatus.rhymes)}
+                            </ul>
+                        </Card.Body>
+                    </Card>
 
-                {/* synonyms card */}
-                <Card className="synonms-card">
-                    <Card.Body>
-                        <Card.Title>Synonyms</Card.Title>
-                        <ul>
-                            {generateList(apiStatus.synonyms)}
-                        </ul>
-                    </Card.Body>
-                </Card>
+                    {/* synonyms card */}
+                    <Card className="synonms-card col-12 col-md-3 my-2">
+                        <Card.Body>
+                            <Card.Title className="p-0 m-3">Synonyms</Card.Title>
+                            <ul className="list-group">
+                                {generateList(apiStatus.synonyms)}
+                            </ul>
+                        </Card.Body>
+                    </Card>
 
-                {/* antonyms card */}
-                <Card className="antonyms-card">
-                    <Card.Body>
-                        <Card.Title>Antonyms</Card.Title>
-                        <ul>
-                            {generateList(apiStatus.antonyms)}
-                        </ul>
-                    </Card.Body>
-                </Card>
-            </div>
-        </Card.Body>
-      </Card>
+                    {/* antonyms card */}
+                    <Card className="antonyms-card col-12 col-md-3 my-2">
+                        <Card.Body>
+                            <Card.Title className="p-0 m-3">Antonyms</Card.Title>
+                            <ul className="list-group">
+                                {generateList(apiStatus.antonyms)}
+                            </ul>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </Card.Body>
+        </Card>
+      </Row>
+      
     );
   }
 }
