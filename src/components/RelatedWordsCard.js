12c5369543a099063cba3d4e8ea6f28a0d879636
Nick <72912456+nickmbk@users.noreply.github.com>
2023-02-18 18:58:40 +0000

    add synomyms card

--- src/components/RelatedWordsCard.js
+++ src/components/RelatedWordsCard.js
@@ -2,56 +2,48 @@ import React, { useContext } from "react";
 import { Card } from "react-bootstrap";
 import { SearchContext } from "../context/SearchContext";
 
-function WordCard({ searchError }) {
+function generateList(words) {
+    return (words.map((item) => {
+        return(
+            <li key={item}>{item}</li>
+        )
+    })
+    )
+}
+
+function RelatedWordsCard() {
   const { apiStatus } = useContext(SearchContext);
 
-  if (searchError) {
-    // If there is a search error, display the error message in a yellow alert
+  // if there is no api information display nothing
+  if (Object.keys(apiStatus).length === 0) {
     return (
-      <Alert variant="warning" className="mt-3">
-        {searchError}
-      </Alert>
-    );
-  } else if (Object.keys(apiStatus).length === 0) {
-    // If there is no data yet, display a message to the user in a blue alert
-    return (
-      <Alert variant="primary" className="mt-3">
-        Please enter a search term above.
-      </Alert>
-    );
-  } else if (apiStatus.success === false) {
-    // If the API call was not successful, display an error message in a red alert
-    return (
-      <Alert variant="danger" className="mt-3">
-        No results for your search. Please try another word.
-      </Alert>
-    );
+      <div></div>
+    )
   } else {
-    // Otherwise, display the word breakdown
+    console.log(apiStatus);
+    // Otherwise, display the synonyms, antonyms and rhyming words
     return (
-      <Card>
+      <Card className="related-words-card">
         <Card.Body>
-          <Card.Title>{apiStatus.word}</Card.Title>
+          <Card.Title>Related Words</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
-            {apiStatus.partOfSpeech}
           </Card.Subtitle>
           <Card.Text>{apiStatus.definition}</Card.Text>
-          <Card.Text>
-            <strong>Syllables:</strong> {apiStatus.syllables.join("-")}
-          </Card.Text>
-          <Card.Text>
-            <strong>Synonyms:</strong> {apiStatus.synonyms.join(", ")}
-          </Card.Text>
-          <Card.Text>
-            <strong>Antonyms:</strong> {apiStatus.antonyms.join(", ")}
-          </Card.Text>
-          <Card.Text>
-            <strong>Rhyming words:</strong> {apiStatus.rhymes.join("-")}
-          </Card.Text>
+            
+            {/* synonyms card */}
+            <Card className="synonms-card">
+                <Card.Body>
+                    <Card.Title>Synonyms</Card.Title>
+                    <ul>
+                        {generateList(apiStatus.synonyms)}
+                    </ul>
+                </Card.Body>
+            </Card>
+
         </Card.Body>
       </Card>
     );
   }
 }
 
-export default WordCard;
+export default RelatedWordsCard;
