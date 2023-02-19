227c1446ffa88bede00b8e21d56f56ab02e1b5c6
ad0m <adamweldon@gmail.com>
2023-02-19 13:47:27 +0000

    Completing Pexels API

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -8,12 +8,14 @@ import { SearchContext } from "../context/SearchContext";
 import axios from "axios";
 import Hero from "../components/Hero/Hero";
 import RelatedWordsCard from '../components/RelatedWordsCard/RelatedWordsCard';
+import Picture from "../components/Picture";
 
 function SearchPage() {
   const [searchError, setSearchError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [searchedWord, setSearchedWord] = useState("");
   const [scrabbleScore, setScrabbleScore] = useState(0);
+
 
   function SearchForm() {
     const { searchInput, setSearchInput, setApiStatus } =
@@ -150,6 +152,7 @@ function SearchPage() {
       <WordCard searchError={searchError} scrabbleScore={scrabbleScore} />
       {searchedWord && <Scrabble word={searchedWord} />}
       <RelatedWordsCard />
+      <Picture word={searchedWord} />
     </div>
   );
 }
