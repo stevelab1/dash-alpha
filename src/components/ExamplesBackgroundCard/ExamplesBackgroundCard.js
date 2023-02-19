c3c8d005ccb0573c96573e8733c3573aa08b61c7
Nick <72912456+nickmbk@users.noreply.github.com>
2023-02-19 11:02:12 +0000

    add bootstrap classes and custom css to style card

--- src/components/ExamplesBackgroundCard/ExamplesBackgroundCard.js
+++ src/components/ExamplesBackgroundCard/ExamplesBackgroundCard.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Row, Card } from "react-bootstrap";
 import { SearchContext } from "../../context/SearchContext";
 
-// import "./RelatedWordsCard.css";
+import "./ExamplesBackgroundCard.css";
 
 function generateList(words) {
   return words.map((item) => {
@@ -18,8 +18,15 @@ function ExamplesCard() {
   const { apiStatus } = useContext(SearchContext);
 
   // If there is background information then add a class to display it, otherwise add a class to hide it
-  let displayBackground = "invisible";
-  apiStatus.background ? displayBackground = "visible" : displayBackground = 'invisible';
+  let displayBackground;
+  let displayExamples;
+  if (apiStatus.background) {
+    displayBackground = "d-block";
+    displayExamples = "col-lg-5";
+  } else {
+    displayBackground = "d-none";
+    displayExamples = "col-lg-7 mx-auto";
+  }
 
   // if there is no api information display nothing
   if (Object.keys(apiStatus).length === 0) {
@@ -28,29 +35,45 @@ function ExamplesCard() {
     // Otherwise, display the examples
     return (
       <Row className="mx-0 my-2">
-        <Card className="examples-background-card col-12 col-lg-12 p-3 mx-auto border-0">
+        <Card className="examples-background-card col-12 p-3 mx-auto border-0">
           <Card.Body className="p-0 m-0">
-            <div>
-                <Card className="examples-card">
-                  <Card.Body>
-                    <Card.Title className="text-start">Use {apiStatus.word} in a sentence</Card.Title>
-                      <ul>
-                        {/* {generateList(apiStatus.examples)} */}
-                        {generateList(['This is an example sentence for testing.','This is another example sentence.', 'But these sentences are purely for testing.', 'The line above this in the code is the real line when the api call has been added in'])}
-                      </ul>
-                  </Card.Body>
-                </Card>
+            <div className="d-flex flex-column flex-lg-row justify-content-evenly">
+              {/* examples card */}
+              <Card
+                className={`examples-card col-12 my-2 border-0 ${displayExamples}`}
+              >
+                <Card.Body>
+                  <Card.Title className="p-0 mb-3 text-start">
+                    Use {apiStatus.word} in a sentence
+                  </Card.Title>
+                  <ul className="list-group">
+                    {/* {generateList(apiStatus.examples)} */}
+                    {generateList([
+                      "This is an example sentence for testing.",
+                      "This is another example sentence.",
+                      "But these sentences are purely for testing.",
+                      "The line above this in the code is the real line when the api call has been added in",
+                    ])}
+                  </ul>
+                </Card.Body>
+              </Card>
 
-                <Card className={`background-card ${displayBackground}`}>
-                  <Card.Body>
-                    <Card.Title className="text-start">Background of the word {apiStatus.word}</Card.Title>
-                      <Card.Text>
-                        {apiStatus.background}
-                      </Card.Text>
-                  </Card.Body>
-                </Card>
+              <div
+                className={`divider border-top border-bottom border-start border-end ${displayBackground}`}
+              ></div>
+
+              {/* background card */}
+              <Card
+                className={`background-card col-12 col-lg-5 my-2 ${displayBackground}`}
+              >
+                <Card.Body>
+                  <Card.Title className="text-start p-0 mb-3">
+                    Background of the word {apiStatus.word}
+                  </Card.Title>
+                  <Card.Text>{apiStatus.background}</Card.Text>
+                </Card.Body>
+              </Card>
             </div>
-            
           </Card.Body>
         </Card>
       </Row>
