b880b9b6d807213ef4fe5e395c0e5ad4554be3ff
Nick <72912456+nickmbk@users.noreply.github.com>
2023-02-19 12:29:25 +0000

    add bootstrap classes and custom css, installed use-sound hook from npm library to play sound in react, modified package.json

--- src/components/PronounciationCard/PronounciationCard.js
+++ src/components/PronounciationCard/PronounciationCard.js
@@ -20,15 +20,15 @@ function RelatedWordsCard() {
     // Otherwise, display the synonyms, antonyms and rhyming words
     return (
       <Row className="mx-0 my-2">
-        <Card className="pronounciation-card col-12 col-lg-12 p-3 mx-auto border-0 text-start">
+        <Card className="pronounciation-card col-12 col-lg-12 p-3 mx-auto border-0 text-center text-md-start">
           <Card.Body className="p-0 m-0">
-          <div className="d-flex flex-column flex-md-row justify-content-evenly pb-3">
-            <div className="syllables">
-              <h3>Ready to pronounce it?</h3>
-              <p className="syllable-breakdown">{apiStatus.syllables.join(' - ')}</p>
+          <div className="d-flex flex-column flex-md-row justify-content-center pb-3">
+            <div className="syllables col-12 col-md-4 d-flex flex-column justify-content-center">
+              <h3 className="m-2 p-0">Ready to pronounce it?</h3>
+              <p className="syllable-breakdown m-2 p-0">{apiStatus.syllables.join(' - ')}</p>
             </div>
-            <div className="sound">
-              <button className="play-button rounded" onClick={handleClick}>Play    <i className="fas fa-volume-high"></i></button>
+            <div className="sound col-12 col-md-4 d-flex justify-content-center justify-content-md-end align-items-center">
+              <button className="play-button rounded px-3 col-12 col-md-8 col-lg-6" onClick={handleClick}>Play! 🔊</button>
             </div>
           </div>
           </Card.Body>
