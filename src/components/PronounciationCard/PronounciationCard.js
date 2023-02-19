a1475a438420999da728166f82ca3e7656b3af6d
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2023-02-19 23:34:35 +0000

    pronunciation card working on serahcpage for some words

--- src/components/PronounciationCard/PronounciationCard.js
+++ src/components/PronounciationCard/PronounciationCard.js
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
 import { SearchContext } from "../../context/SearchContext";
+import axios from "axios";
 
-function PronounciationCard() {
+function PronunciationCard() {
   const { apiStatus } = useContext(SearchContext);
 
   // If phonetics data is not available for the searched word, do not display this card
@@ -9,22 +10,37 @@ function PronounciationCard() {
     return null;
   }
 
-  // const audioUrl = apiStatus.phonetics;
+  const playAudio = async (audioUrl) => {
+    const audio = new Audio(audioUrl);
+    await audio.play();
+  }
+
+  const getAudioUrl = async () => {
+    try {
+      const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${apiStatus.word}`);
+      const audioUrl = response.data[0].phonetics[0].audio;
+      return audioUrl;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
 
   return (
     <div className="card mb-4">
       <div className="card-header">Pronunciation</div>
       <div className="card-body">
-        <audio controls>
-          <source
-            src={`https://api.dictionaryapi.dev/media/pronunciations/en/test-uk.mp3`}
-            type="audio/mpeg"
-          />
-          Your browser does not support the audio element.
-        </audio>
+        <button className="btn btn-primary" onClick={async () => {
+          const audioUrl = await getAudioUrl();
+          if (audioUrl) {
+            playAudio(audioUrl);
+          }
+        }}>
+          Play Audio
+        </button>
       </div>
     </div>
   );
 }
 
-export default PronounciationCard;
+export default PronunciationCard;
