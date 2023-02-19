dcf6c4b21c8981f738005a1b924acc9f08628c1d
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2023-02-19 23:51:23 +0000

    pronunciation card working but vocabulary seems limited

--- src/components/PronounciationCard/PronounciationCard.js
+++ src/components/PronounciationCard/PronounciationCard.js
@@ -13,29 +13,34 @@ function PronunciationCard() {
   const playAudio = async (audioUrl) => {
     const audio = new Audio(audioUrl);
     await audio.play();
-  }
+  };
 
   const getAudioUrl = async () => {
     try {
-      const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${apiStatus.word}`);
+      const response = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${apiStatus.word}`
+      );
       const audioUrl = response.data[0].phonetics[0].audio;
       return audioUrl;
     } catch (error) {
       console.error(error);
       return null;
     }
-  }
+  };
 
   return (
     <div className="card mb-4">
       <div className="card-header">Pronunciation</div>
       <div className="card-body">
-        <button className="btn btn-primary" onClick={async () => {
-          const audioUrl = await getAudioUrl();
-          if (audioUrl) {
-            playAudio(audioUrl);
-          }
-        }}>
+        <button
+          className="btn btn-primary"
+          onClick={async () => {
+            const audioUrl = await getAudioUrl();
+            if (audioUrl) {
+              playAudio(audioUrl);
+            }
+          }}
+        >
           Play Audio
         </button>
       </div>
