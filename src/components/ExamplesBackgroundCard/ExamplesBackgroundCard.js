ef67fcfde5a173fc0b1e35260081e308e4973527
Nick <72912456+nickmbk@users.noreply.github.com>
2023-02-19 09:39:58 +0000

    add a variable and ternary operator to decide whether to display a card or not depending on information gathered from api

--- src/components/ExamplesBackgroundCard/ExamplesBackgroundCard.js
+++ src/components/ExamplesBackgroundCard/ExamplesBackgroundCard.js
@@ -17,6 +17,10 @@ function generateList(words) {
 function ExamplesCard() {
   const { apiStatus } = useContext(SearchContext);
 
+  // If there is background information then add a class to display it, otherwise add a class to hide it
+  let displayBackground = "invisible";
+  apiStatus.background ? displayBackground = "visible" : displayBackground = 'invisible';
+
   // if there is no api information display nothing
   if (Object.keys(apiStatus).length === 0) {
     return <div></div>;
@@ -34,6 +38,15 @@ function ExamplesCard() {
                         {/* {generateList(apiStatus.examples)} */}
                         {generateList(['This is an example sentence for testing.','This is another example sentence.', 'But these sentences are purely for testing.', 'The line above this in the code is the real line when the api call has been added in'])}
                       </ul>
+                  </Card.Body>
+                </Card>
+
+                <Card className={`background-card ${displayBackground}`}>
+                  <Card.Body>
+                    <Card.Title className="text-start">Background of the word {apiStatus.word}</Card.Title>
+                      <Card.Text>
+                        {apiStatus.background}
+                      </Card.Text>
                   </Card.Body>
                 </Card>
             </div>
