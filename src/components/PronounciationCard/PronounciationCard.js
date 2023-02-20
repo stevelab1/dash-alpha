e55ce637661e384d8f4c5353b977e9e98ef53429
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2023-02-20 04:06:23 +0000

    PronunciationCard limited case error handling, SVG sizing

--- src/components/PronounciationCard/PronounciationCard.js
+++ src/components/PronounciationCard/PronounciationCard.js
@@ -1,10 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { SearchContext } from "../../context/SearchContext";
 import axios from "axios";
-import { Card, Row, Button } from "react-bootstrap";
+import { Card, Row, Button, Alert } from "react-bootstrap";
 
 function PronunciationCard() {
   const { apiStatus } = useContext(SearchContext);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   if (!apiStatus?.phonetics) {
     return null;
@@ -12,16 +13,26 @@ function PronunciationCard() {
 
   const handleClick = async () => {
     try {
+      setErrorMessage(null); // reset error message
       const response = await axios.get(
         `https://api.dictionaryapi.dev/api/v2/entries/en/${apiStatus.word}`
       );
-      const audioUrl = response.data[0].phonetics[0].audio;
+      const phonetics = response.data[0]?.phonetics;
+      if (!phonetics) {
+        throw new Error("Unable to find phonetics for this word.");
+      }
+      const audioUrl = phonetics[0].audio;
       const audio = new Audio(audioUrl);
+      audio.addEventListener("error", () => {
+        setErrorMessage("Unable to play audio pronunciation.");
+      });
       await audio.play();
     } catch (error) {
       console.error(error);
+      setErrorMessage("Unable to load pronunciation. Try another word.");
     }
   };
+  
 
   return (
     <Row className="mx-0 my-2">
@@ -38,18 +49,24 @@ function PronunciationCard() {
               <Button onClick={handleClick} variant="outline-dark">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width="16"
+                  width="24"
                   height="16"
                   fill="currentColor"
                   className="bi bi-play-btn"
-                  viewBox="0 0 16 16"
+                  viewBox="0 1 24 16"
                 >
                   <path d="M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
                   <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
-                </svg>{" "}
+                </svg>
                 Play
               </Button>
             </div>
+
+            {errorMessage && (
+              <Alert variant="warning" className="mx-4 mt-4 border-rounded">
+                <p className="px-0 py-2 m-0 border-rounded">{errorMessage}</p>
+              </Alert>
+            )}
           </div>
         </Card.Body>
       </Card>
